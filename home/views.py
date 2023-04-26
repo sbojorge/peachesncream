d83d90a2ff646eb435219040a3fddc02ff6fbbac
sbojorge <sara.bojorge@gmail.com>
2023-04-26 15:08:27 +0000

    create base template and render it to the user

--- home/views.py
+++ home/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+
+class LandingPage(generic.TemplateView):
+    template_name = 'home/landing_page.html'
