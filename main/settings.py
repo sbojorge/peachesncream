41decb983c3509cb6251620b38f5c4c5becfbe3a
sbojorge <sara.bojorge@gmail.com>
2023-04-29 17:59:08 +0000

    deployment commit

--- main/settings.py
+++ main/settings.py
@@ -27,7 +27,9 @@ if os.path.exists('env.py'):
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['newpeachesncream.herokuapp.com', 'localhost']
 
