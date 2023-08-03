3acea4ca4480eddd62e4b1436a0a3565eaa94033
sbojorge <sara.bojorge@gmail.com>
2023-08-03 09:17:30 +0000

    Update the default attribute in the category field on the model

--- grocery_list/models.py
+++ grocery_list/models.py
@@ -18,7 +18,7 @@ class Grocery(models.Model):
 
     name = models.CharField(max_length=500, default='My grocery list')
     category = models.CharField(max_length=20, choices=CATEGORIES,
-                                default='Shopping')
+                                default='groceries')
     shop = models.CharField(max_length=500, blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE,
                              related_name='grocery_owner')
