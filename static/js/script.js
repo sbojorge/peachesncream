f37236a51485c59031ea7fc4894d5dc9754b14e8
sbojorge <sara.bojorge@gmail.com>
2023-08-03 08:17:15 +0000

    Initialize the select element in the form for adding a list

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,11 @@
 // sidebar initialization
-document.addEventListener('DOMContentLoaded', function() {
-    let sidenav = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(sidenav);
-  });
+document.addEventListener("DOMContentLoaded", function () {
+  let sidenav = document.querySelectorAll(".sidenav");
+  M.Sidenav.init(sidenav);
+});
 
-
+// select initialization
+document.addEventListener("DOMContentLoaded", function () {
+  let elems = document.querySelectorAll("select");
+  M.FormSelect.init(elems, options);
+});
