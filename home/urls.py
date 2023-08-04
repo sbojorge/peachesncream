e8631df062afc41d0b077533ecd424c344b49a9b
sbojorge <sara.bojorge@gmail.com>
2023-08-04 18:30:47 +0000

    Add home page, its view, url and update header.html

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import LandingPage
+from .views import LandingPage, HomePage
 
 
 urlpatterns = [
-    path('', LandingPage.as_view(), name='landing')
+    path('', LandingPage.as_view(), name='landing'),
+    path('home/', HomePage.as_view(), name='home')
 ]
--- home/views.py
+++ home/views.py
@@ -3,3 +3,7 @@ from django.views import generic
 
 class LandingPage(generic.TemplateView):
     template_name = 'home/landing_page.html'
+
+
+class HomePage(generic.TemplateView):
+    template_name = 'home/home_page.html'
