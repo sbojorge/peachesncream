432adb024ae1b3dd6ba39825e77dce9e03f16d9f
sbojorge <sara.bojorge@gmail.com>
2023-08-08 19:04:31 +0000

    Add success message after submitting contact form

--- contact/views.py
+++ contact/views.py
@@ -2,16 +2,18 @@ from django.views.generic.edit import CreateView
 from .models import Contact
 from .forms import ContactForm
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
 
 
-class AddContact(LoginRequiredMixin, CreateView):
+class AddContact(LoginRequiredMixin, SuccessMessageMixin, CreateView):
     """
-    Create a contact form view
+    Create a contact form view and add success message into template
     """
     model = Contact
     template_name = 'contact/add_contact.html'
     form_class = ContactForm
     success_url = '/contact/'
+    success_message = "contact form was sent successfully"
 
     def form_valid(self, form):
         form.instance.user = self.request.user
