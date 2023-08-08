26fe84f8cfc3c7f909b07ae1a76e1536df25ef7d
sbojorge <sara.bojorge@gmail.com>
2023-08-08 16:20:41 +0000

    Add success messages to add, edit and update list views

--- grocery_list/views.py
+++ grocery_list/views.py
@@ -4,29 +4,34 @@ from django.views.generic.list import ListView
 from .models import Grocery
 from .forms import GroceryForm
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.messages.views import SuccessMessageMixin
 
 
-class DeleteList(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+class DeleteList(LoginRequiredMixin, UserPassesTestMixin,
+                 SuccessMessageMixin, DeleteView):
     """
     Delete a shopping list
     """
     model = Grocery
     template_name = 'grocery_list/grocery_list_confirm_delete.html'
     context_object_name = 'grocery'
-    success_url = '/grocery_list/'
+    success_url = '/grocery_list/index'
+    success_message = "shopping list was deleted successfully"
 
     def test_func(self):
         return self.request.user == self.get_object().user
 
 
-class EditList(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+class EditList(LoginRequiredMixin, UserPassesTestMixin, SuccessMessageMixin,
+               UpdateView):
     """
     Edit a shopping list
     """
     model = Grocery
     template_name = 'grocery_list/edit_list.html'
     form_class = GroceryForm
-    success_url = '/grocery_list/'
+    success_url = '/grocery_list/index'
+    success_message = "shopping list was updated successfully"
 
     def test_func(self):
         return self.request.user == self.get_object().user
@@ -57,14 +62,15 @@ class Index(LoginRequiredMixin, ListView):
         return grocerys
 
 
-class AddList(LoginRequiredMixin, CreateView):
+class AddList(LoginRequiredMixin, SuccessMessageMixin, CreateView):
     """
-    Create a shopping list
+    Create a shopping list and add success message into template
     """
     model = Grocery
     template_name = 'grocery_list/add_list.html'
     form_class = GroceryForm
     success_url = '/grocery_list/index'
+    success_message = "shopping list was created successfully"
 
     def form_valid(self, form):
         form.instance.user = self.request.user
