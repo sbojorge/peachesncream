70a1027b4fb210df93e3365cef509b2be5e3b810
sbojorge <sara.bojorge@gmail.com>
2025-08-07 20:57:44 +0200

    Refactor DATABASE section

--- main/settings.py
+++ main/settings.py
@@ -126,20 +126,9 @@ WSGI_APPLICATION = 'main.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# Simplified DATABASES configuration using dj-database-url
-# This will automatically use the DATABASE_URL environment variable if it exists.
-# If it doesn't exist (e.g., in your local development environment), it will fall back to SQLite.
-if 'DATABASE_URL' in os.environ:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
