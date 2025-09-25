e1f374e1cc710a13915351d0686911f23d5b0411
sbojorge <sara.bojorge@gmail.com>
2025-09-25 19:47:33 +0200

    Update ALLOWED_HOSTS

--- main/settings.py
+++ main/settings.py
@@ -36,7 +36,7 @@ DEBUG = True
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 # Application definition
 
