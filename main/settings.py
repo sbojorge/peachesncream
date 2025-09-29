e38a4f13ec1cbde99cef50a17e295125ecb40c65
sbojorge <sara.bojorge@gmail.com>
2025-09-29 18:39:05 +0200

    Update allowed_hosts section for respecting only 2 values

--- main/settings.py
+++ main/settings.py
@@ -36,7 +36,11 @@ DEBUG = True
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
-ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', 'localhost,127.0.0.1', 'peachesncream-782513543316.europe-southwest1.run.app').split(',')
+# CORRECTED settings.py line
+ALLOWED_HOSTS = os.environ.get(
+    'DJANGO_ALLOWED_HOSTS',
+    'localhost,127.0.0.1,peachesncream-782513543316.europe-southwest1.run.app'
+).split(',')
 
 # Application definition
 
